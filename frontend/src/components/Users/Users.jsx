43028757ba63a4c2e30b3519b0e53dbf5377cb30
ChristianPuyo <christianpuyotorres@gmail.com>
2023-10-31 21:33:05 -0500

    últimos cambios

--- frontend/src/components/Users/Users.jsx
+++ frontend/src/components/Users/Users.jsx
@@ -13,6 +13,19 @@ const Users = ()=>{
     return(
         <div>
             <h1>Users</h1>
+            {
+                users.map(element =>{
+                    return(
+                        <div>
+                            <h3>Name:{element.name}</h3>
+                            <h3>Email:{element.email}</h3>
+                            <h3>Ciudad:{element.address.city}</h3>
+                            <hr></hr>
+                        </div>
+                        
+                    )
+                })
+            }
         </div>
     )
 }
